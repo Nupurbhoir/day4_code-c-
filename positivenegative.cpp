7da18143ecccd4f6c818a8790c05374fa82facfd
Nupurbhoir <nupurbhoir21@gmail.com>
2024-11-30 10:38:57 +0530

    Add files via upload
    try this basic codes

---
+++ positivenegative.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int number; 
+    cout<< "Enter a number" <<endl;
+    cin>>number;
+    if(number>0){
+        cout<<"Number is positive "<<endl;
+    }
+    else if(number <0){  
+        cout<<"Number is negative  "<<endl;
+    }
+
+    
+return 0;
+}
